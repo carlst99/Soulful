1246651cbf47751956a38dec5ec01c108694ae22
Carl Stephens <carlst99@outlook.co.nz>
2019-07-09 08:39:31 +1200

    Add project files.

---
+++ Soulful.Wpf/Views/HomeView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MvvmCross.Platforms.Wpf.Views;
+
+namespace Soulful.Wpf.Views
+{
+    public partial class HomeView : MvxWpfView
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
