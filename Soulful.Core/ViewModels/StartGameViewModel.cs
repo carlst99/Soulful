5ce97c34f1bed24d3e62692d741a6a39c2194eac
Carl Stephens <carlst99@outlook.co.nz>
2019-07-10 10:53:29 +1200

    Bind all strings with OneTime

---
+++ Soulful.Core/ViewModels/StartGameViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MvvmCross.Navigation;
+
+namespace Soulful.Core.ViewModels
+{
+    public class StartGameViewModel : Base.ViewModelBase
+    {
+        private int _gamePin;
+
+        public int GamePin
+        {
+            get => _gamePin;
+            set => SetProperty(ref _gamePin, value);
+        }
+
+        public StartGameViewModel(IMvxNavigationService navigationService)
+            : base(navigationService)
+        {
+        }
+    }
+}
