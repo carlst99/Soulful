935fcbe4a9530d58b5411fae579c20aabbbd1668
Carl Stephens <carlst99@outlook.co.nz>
2019-07-09 13:04:12 +1200

    Create CardBrowserView, only populated with dummy cards at the moment

--- Soulful.Core/Resources/AppStrings.Designer.cs
+++ Soulful.Core/Resources/AppStrings.Designer.cs
@@ -8,11 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Soulful.Core.Resources
-{
+namespace Soulful.Core.Resources {
     using System;
-
-
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -23,49 +22,122 @@ namespace Soulful.Core.Resources
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class AppStrings
-    {
-
+    public class AppStrings {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal AppStrings()
-        {
+        internal AppStrings() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if (object.ReferenceEquals(resourceMan, null))
-                {
+        public static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Soulful.Core.Resources.AppStrings", typeof(AppStrings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        public static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to You&apos;ve been exposed.
+        /// </summary>
+        public static string App_Subtitle {
+            get {
+                return ResourceManager.GetString("App_Subtitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Soulful.
+        /// </summary>
+        public static string App_Title {
+            get {
+                return ResourceManager.GetString("App_Title", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Browse Cards.
+        /// </summary>
+        public static string Command_BrowseCards {
+            get {
+                return ResourceManager.GetString("Command_BrowseCards", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Join Game.
+        /// </summary>
+        public static string Command_JoinGame {
+            get {
+                return ResourceManager.GetString("Command_JoinGame", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Start Game.
+        /// </summary>
+        public static string Command_StartGame {
+            get {
+                return ResourceManager.GetString("Command_StartGame", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Black Cards.
+        /// </summary>
+        public static string Header_BlackCards {
+            get {
+                return ResourceManager.GetString("Header_BlackCards", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Card Browser.
+        /// </summary>
+        public static string Header_CardBrowser {
+            get {
+                return ResourceManager.GetString("Header_CardBrowser", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to White Cards.
+        /// </summary>
+        public static string Header_WhiteCards {
+            get {
+                return ResourceManager.GetString("Header_WhiteCards", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Go Back.
+        /// </summary>
+        public static string ToolTip_NavigateBack {
+            get {
+                return ResourceManager.GetString("ToolTip_NavigateBack", resourceCulture);
             }
         }
     }
---
+++ Soulful.Core/ViewModels/CardBrowserViewModel.cs
@@ -0,0 +1,36 @@
+﻿using MvvmCross.Commands;
+using MvvmCross.Navigation;
+using System.Collections.ObjectModel;
+
+namespace Soulful.Core.ViewModels
+{
+    public class CardBrowserViewModel : Base.ViewModelBase
+    {
+        private ObservableCollection<string> _whiteCards;
+        private ObservableCollection<string> _blackCards;
+
+        public ObservableCollection<string> WhiteCards
+        {
+            get => _whiteCards;
+            set => SetProperty(ref _whiteCards, value);
+        }
+
+        public ObservableCollection<string> BlackCards
+        {
+            get => _blackCards;
+            set => SetProperty(ref _blackCards, value);
+        }
+
+        public IMvxCommand NavigateBackCommand => new MvxCommand(() => NavigationService.Navigate<HomeViewModel>());
+
+        public CardBrowserViewModel(IMvxNavigationService navigationService)
+            : base(navigationService)
+        {
+            WhiteCards = new ObservableCollection<string>();
+            BlackCards = new ObservableCollection<string>();
+
+            for (int i = 0; i < 10; i++)
+                WhiteCards.Add("test" + i.ToString());
+        }
+    }
+}
