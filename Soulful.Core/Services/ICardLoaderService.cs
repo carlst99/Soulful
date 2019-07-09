92fac4ddb9b9c32bc840f95f89d4eb34c1b6ea78
Carl Stephens <carlst99@outlook.co.nz>
2019-07-09 16:52:20 +1200

    Add card loader service - changing card format

---
+++ Soulful.Core/Services/ICardLoaderService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Soulful.Core.Services
+{
+    public interface ICardLoaderService
+    {
+        List<string> PackKeys { get; }
+        Dictionary<string, string> PackNames { get; }
+    }
+}
