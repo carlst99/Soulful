dcf019d806fc1ce2b9b987046e99c4defd2ccabd
Carl Stephens <carlst99@outlook.co.nz>
2019-07-17 13:13:42 +1200

    Show message box in mainwindow

--- Soulful.Core/Model/DialogMessage.cs
+++ Soulful.Core/Model/DialogMessage.cs
@@ -5,9 +5,13 @@ namespace Soulful.Core.Model
 {
     public class DialogMessage : Message
     {
+        [Flags]
         public enum Button
         {
-            OK, YesNo, OKCancel, YesNoCancel
+            Ok = 2,
+            Yes = 4,
+            No = 8,
+            Cancel = 16
         }
 
         public string Title { get; set; }
--- Soulful.Wpf/MainWindow.xaml.cs
+++ Soulful.Wpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using IntraMessaging;
+﻿using AmRoMessageDialog;
+using IntraMessaging;
 using MvvmCross;
 using MvvmCross.Platforms.Wpf.Views;
 using Soulful.Core.Model;
@@ -8,9 +9,26 @@ namespace Soulful.Wpf
 {
     public partial class MainWindow : MvxWindow
     {
+        private readonly AmRoMessageBox _messageBox;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _messageBox = new AmRoMessageBox
+            {
+                Background = "#333",
+                TextColor = "#fff",
+                RippleEffectColor = "#000",
+                ClickEffectColor = "#1F2023",
+                ShowMessageWithEffect = true,
+                EffectArea = this,
+                ParentWindow = this,
+                IconColor = "#3399ff",
+                CaptionFontSize = 16,
+                MessageFontSize = 14
+            };
+
             IIntraMessenger messenger = Mvx.IoCProvider.Resolve<IIntraMessenger>();
             messenger.Subscribe(OnMessage, new Type[] { typeof(DialogMessage) });
         }
@@ -19,7 +37,36 @@ namespace Soulful.Wpf
         {
             if (message is DialogMessage dMessage)
             {
+                AmRoMessageBoxButton buttons = AmRoMessageBoxButton.Ok;
+                if ((dMessage.Buttons & DialogMessage.Button.Ok) != 0)
+                {
+                    if ((dMessage.Buttons & DialogMessage.Button.Yes) != 0)
+                        buttons = AmRoMessageBoxButton.OkCancel;
+                    else
+                        buttons = AmRoMessageBoxButton.Ok;
+                } else if ((dMessage.Buttons & DialogMessage.Button.Yes) != 0)
+                {
+                    if ((dMessage.Buttons & DialogMessage.Button.Yes) != 0)
+                        buttons = AmRoMessageBoxButton.YesNoCancel;
+                    else
+                        buttons = AmRoMessageBoxButton.YesNo;
+                }
 
+                switch (_messageBox.Show(dMessage.Content, dMessage.Title, buttons))
+                {
+                    case AmRoMessageBoxResult.Cancel:
+                        dMessage.Callback.Invoke(DialogMessage.Button.Cancel);
+                        break;
+                    case AmRoMessageBoxResult.No:
+                        dMessage.Callback.Invoke(DialogMessage.Button.No);
+                        break;
+                    case AmRoMessageBoxResult.Ok:
+                        dMessage.Callback.Invoke(DialogMessage.Button.Ok);
+                        break;
+                    case AmRoMessageBoxResult.Yes:
+                        dMessage.Callback.Invoke(DialogMessage.Button.Yes);
+                        break;
+                }
             }
         }
     }
