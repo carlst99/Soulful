cfd137fc12244abc668d2021252c96062c11b974
Carl Stephens <carlst99@outlook.co.nz>
2019-08-03 16:55:06 +1200

    Create Player class

---
+++ Soulful.Core/Model/Player.cs
@@ -0,0 +1,27 @@
+﻿using LiteNetLib;
+using System;
+using System.Collections.Generic;
+
+namespace Soulful.Core.Model
+{
+    public class Player
+    {
+        public NetPeer Peer { get; set; }
+        public List<int> WhiteCards { get; set; }
+        public int TotalScore { get; set; }
+        public string Name { get; set; }
+
+        public Player()
+            : this(null, "Player" + new Random().Next(1000))
+        {
+        }
+
+        public Player(NetPeer peer, string name)
+        {
+            Peer = peer;
+            Name = name;
+            WhiteCards = new List<int>();
+            TotalScore = 0;
+        }
+    }
+}
