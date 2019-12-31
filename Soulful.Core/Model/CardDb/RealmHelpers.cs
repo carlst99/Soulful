3a8359036d6d6e8838348881fcb6d4d27e047414
Carl Stephens <carlst99@outlook.co.nz>
2019-12-31 15:31:54 +1300

    Add RealmHelpers, remove CardLoaderService

--- Soulful.Core/Model/CardDb/RealmHelpers.cs
+++ Soulful.Core/Model/CardDb/RealmHelpers.cs
@@ -1,41 +1,30 @@
 ﻿using Realms;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Soulful.Core.Model.CardDb
 {
     public static class RealmHelpers
     {
-        private static readonly Dictionary<Type, int> _currentIds = new Dictionary<Type, int>();
-
         public static Realm GetRealmInstance()
         {
             string realmPath = App.GetAppdataFilePath("Soulful.realm");
-            RealmConfiguration config = new RealmConfiguration(realmPath);
+            RealmConfiguration config = new RealmConfiguration(realmPath)
+            {
+                ObjectClasses = new Type[] { typeof(Preferences) }
+            };
             return Realm.GetInstance(config);
         }
 
-        public static int GetNextId<T>(Realm realm = null) where T : RealmObject, ICard
+        public static Realm GetCardsRealm()
         {
-            if (realm == null)
-                realm = GetRealmInstance();
-
-            if (!_currentIds.ContainsKey(typeof(T)))
+            string realmPath = App.GetAppdataFilePath("cards.realm");
+            RealmConfiguration config = new RealmConfiguration(realmPath)
             {
-                int max = 0;
-                foreach (T element in realm.All<T>())
-                {
-                    if (element.Id > max)
-                        max = element.Id;
-                }
-                _currentIds.Add(typeof(T), max);
-            }
-
-            return ++_currentIds[typeof(T)];
+                ObjectClasses = new Type[] { typeof(Pack), typeof(WhiteCard), typeof(BlackCard) }
+            };
+            return Realm.GetInstance(config);
         }
-
-        public static void ClearNextIds() => _currentIds.Clear();
 
         public static Preferences GetUserPreferences(Realm instance = null)
         {
