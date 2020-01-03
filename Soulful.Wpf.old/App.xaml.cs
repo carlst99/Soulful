dbdf45f5b46404284b05a5d0d934130cc1d8926e
Carl Stephens <carlst99@outlook.co.nz>
2020-01-04 08:51:17 +1300

    Bump core to netStandard2.1, create new netCore wpf project and install nuget packages

---
+++ Soulful.Wpf.old/App.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
+using MvvmCross;
+using MvvmCross.Core;
+using MvvmCross.Navigation;
+using MvvmCross.Platforms.Wpf.Core;
+using MvvmCross.Platforms.Wpf.Views;
+using Soulful.Core.Net;
+using Soulful.Core.ViewModels;
+
+namespace Soulful.Wpf
+{
+    public partial class App : MvxApplication
+    {
+        protected override void RegisterSetup()
+        {
+            this.RegisterSetupType<MvxWpfSetup<Core.App>>();
+        }
+
+#if DEBUG
+        private StartupEventArgs _startArgs;
+        private Process _otherProcess;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _startArgs = e;
+            base.OnStartup(e);
+        }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            if (_startArgs?.Args.Length > 0)
+            {
+                IMvxNavigationService navService = Mvx.IoCProvider.Resolve<IMvxNavigationService>();
+
+                if (_startArgs.Args[0] == "autoServer")
+                {
+                    navService.Navigate<StartGameViewModel, string>("Server");
+                    if (_startArgs.Args.Length > 1 && _startArgs.Args[1] == "launchOther")
+                        _otherProcess = Process.Start("Soulful.Wpf.exe", "autoClient");
+                }
+                else if (_startArgs.Args[0] == "autoClient")
+                {
+                    navService.Navigate<JoinGameViewModel, string>("Client");
+                    if (_startArgs.Args.Length > 1 && _startArgs.Args[1] == "launchOther")
+                        _otherProcess = Process.Start("Soulful.Wpf.exe", "autoServer");
+                }
+                _startArgs = null;
+            }
+            base.OnActivated(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if (_otherProcess?.HasExited == false)
+                _otherProcess.CloseMainWindow();
+            base.OnExit(e);
+
+            INetServerService server = Mvx.IoCProvider.Resolve<INetServerService>();
+            if (server.IsRunning)
+                server.Stop();
+
+            INetClientService client = Mvx.IoCProvider.Resolve<INetClientService>();
+            if (client.IsRunning)
+                client.Stop();
+        }
+#else
+        protected override void OnExit(ExitEventArgs e)
+        {
+            INetServerService server = Mvx.IoCProvider.Resolve<INetServerService>().;
+            if (server.IsRunning)
+                server.Stop();
+
+            INetClientService client = Mvx.IoCProvider.Resolve<INetClientService>().;
+            if (client.IsRunning)
+                client.Stop();
+        }
+#endif
+    }
+}
--- Soulful.Wpf/MainWindow.xaml.cs
+++ Soulful.Wpf/MainWindow.xaml.cs
@@ -1,75 +1,28 @@
-﻿using AmRoMessageDialog;
-using IntraMessaging;
-using MvvmCross;
-using MvvmCross.Platforms.Wpf.Views;
-using Soulful.Core.Model;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace Soulful.Wpf
 {
-    public partial class MainWindow : MvxWindow
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        private readonly AmRoMessageBox _messageBox;
-
         public MainWindow()
         {
             InitializeComponent();
-
-            _messageBox = new AmRoMessageBox
-            {
-                Background = "#333",
-                TextColor = "#fff",
-                RippleEffectColor = "#000",
-                ClickEffectColor = "#1F2023",
-                ShowMessageWithEffect = true,
-                EffectArea = this,
-                ParentWindow = this,
-                IconColor = "#3399ff",
-                CaptionFontSize = 16,
-                MessageFontSize = 14
-            };
-
-            IIntraMessenger messenger = Mvx.IoCProvider.Resolve<IIntraMessenger>();
-            messenger.Subscribe(OnMessage, new Type[] { typeof(DialogMessage) });
-        }
-
-        private void OnMessage(IMessage message)
-        {
-            if (message is DialogMessage dMessage)
-            {
-                AmRoMessageBoxButton buttons = AmRoMessageBoxButton.Ok;
-                if ((dMessage.Buttons & DialogMessage.Button.Ok) != 0)
-                {
-                    if ((dMessage.Buttons & DialogMessage.Button.Cancel) != 0)
-                        buttons = AmRoMessageBoxButton.OkCancel;
-                    else
-                        buttons = AmRoMessageBoxButton.Ok;
-                } else if ((dMessage.Buttons & DialogMessage.Button.Yes) != 0)
-                {
-                    if ((dMessage.Buttons & DialogMessage.Button.Cancel) != 0)
-                        buttons = AmRoMessageBoxButton.YesNoCancel;
-                    else
-                        buttons = AmRoMessageBoxButton.YesNo;
-                }
-
-                AmRoMessageBoxResult result = Dispatcher.Invoke(() => _messageBox.Show(dMessage.Content, dMessage.Title, buttons));
-
-                switch (result)
-                {
-                    case AmRoMessageBoxResult.Cancel:
-                        dMessage.Callback?.Invoke(DialogMessage.Button.Cancel);
-                        break;
-                    case AmRoMessageBoxResult.No:
-                        dMessage.Callback?.Invoke(DialogMessage.Button.No);
-                        break;
-                    case AmRoMessageBoxResult.Ok:
-                        dMessage.Callback?.Invoke(DialogMessage.Button.Ok);
-                        break;
-                    case AmRoMessageBoxResult.Yes:
-                        dMessage.Callback?.Invoke(DialogMessage.Button.Yes);
-                        break;
-                }
-            }
         }
     }
 }
