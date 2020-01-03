0ae06d4e9d1d2f7b430df1e0e3782cbd41e2a93c
Carl Stephens <carlst99@outlook.co.nz>
2020-01-04 09:23:37 +1300

    Fix messagebox issues

--- Soulful.Wpf/MainWindow.xaml.cs
+++ Soulful.Wpf/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using MvvmCross;
 using MvvmCross.Platforms.Wpf.Views;
 using Soulful.Core.Model;
 using System;
+using System.Windows;
 
 namespace Soulful.Wpf
 {
@@ -20,7 +21,37 @@ namespace Soulful.Wpf
         {
             if (message is DialogMessage dMessage)
             {
-                //TODO
+                MessageBoxButton buttons = MessageBoxButton.OK;
+                if ((dMessage.Buttons & DialogMessage.Button.Ok) != 0)
+                {
+                    if ((dMessage.Buttons & DialogMessage.Button.Cancel) != 0)
+                        buttons = MessageBoxButton.OKCancel;
+                    else
+                        buttons = MessageBoxButton.OK;
+                }
+                else if ((dMessage.Buttons & DialogMessage.Button.Yes) != 0)
+                {
+                    if ((dMessage.Buttons & DialogMessage.Button.Cancel) != 0)
+                        buttons = MessageBoxButton.YesNoCancel;
+                    else
+                        buttons = MessageBoxButton.YesNo;
+                }
+
+                switch (Dispatcher.Invoke(() => MessageBox.Show(dMessage.Content, dMessage.Title, buttons)))
+                {
+                    case MessageBoxResult.Cancel:
+                        dMessage.Callback?.Invoke(DialogMessage.Button.Cancel);
+                        break;
+                    case MessageBoxResult.No:
+                        dMessage.Callback?.Invoke(DialogMessage.Button.No);
+                        break;
+                    case MessageBoxResult.OK:
+                        dMessage.Callback?.Invoke(DialogMessage.Button.Ok);
+                        break;
+                    case MessageBoxResult.Yes:
+                        dMessage.Callback?.Invoke(DialogMessage.Button.Yes);
+                        break;
+                }
             }
         }
     }
