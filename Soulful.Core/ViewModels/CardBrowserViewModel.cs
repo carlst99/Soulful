0b6aa0cb263e3fcc882858d93bda5e2e373e8cfd
Carl Stephens <carlst99@outlook.co.nz>
2020-01-14 10:34:07 +1300

    Update CardBrowserViewModel to use new model

--- Soulful.Core/ViewModels/CardBrowserViewModel.cs
+++ Soulful.Core/ViewModels/CardBrowserViewModel.cs
@@ -1,10 +1,8 @@
 ﻿using MvvmCross.Commands;
 using MvvmCross.Navigation;
+using Soulful.Core.Model.Cards;
 using Soulful.Core.Services;
-using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Threading.Tasks;
 
 namespace Soulful.Core.ViewModels
 {
@@ -12,43 +10,23 @@ namespace Soulful.Core.ViewModels
     {
         #region Fields
 
-        private readonly ICardLoaderService _cardLoader;
-
-        private ObservableCollection<string> _whiteCards;
-        private ObservableCollection<Tuple<string, int>> _blackCards;
-        private List<PackInfo> _cardPacks;
-        private PackInfo _selectedPack;
+        private List<Pack> _cardPacks;
+        private Pack _selectedPack;
 
         #endregion
 
         #region Properties
 
-        public ObservableCollection<string> WhiteCards
-        {
-            get => _whiteCards;
-            set => SetProperty(ref _whiteCards, value);
-        }
-
-        public ObservableCollection<Tuple<string, int>> BlackCards
-        {
-            get => _blackCards;
-            set => SetProperty(ref _blackCards, value);
-        }
-
-        public List<PackInfo> CardPacks
+        public List<Pack> CardPacks
         {
             get => _cardPacks;
             set => SetProperty(ref _cardPacks, value);
         }
 
-        public PackInfo SelectedPack
+        public Pack SelectedPack
         {
             get => _selectedPack;
-            set
-            {
-                SetProperty(ref _selectedPack, value);
-                LoadCards(value.Key);
-            }
+            set => SetProperty(ref _selectedPack, value);
         }
 
         #endregion
@@ -58,29 +36,9 @@ namespace Soulful.Core.ViewModels
         public CardBrowserViewModel(IMvxNavigationService navigationService, ICardLoaderService cardLoader)
             : base(navigationService)
         {
-            _cardLoader = cardLoader;
             CardPacks = cardLoader.Packs;
             if (CardPacks.Count > 0)
                 SelectedPack = CardPacks[0];
         }
-
-        #region Card Loading
-
-        private async void LoadCards(string packKey)
-        {
-            await Task.WhenAll(LoadBlackCards(packKey), LoadWhiteCards(packKey)).ConfigureAwait(false);
-        }
-
-        private async Task LoadBlackCards(string packKey)
-        {
-            BlackCards = new ObservableCollection<Tuple<string, int>>(await _cardLoader.GetPackBlackCardsAsync(packKey).ConfigureAwait(false));
-        }
-
-        private async Task LoadWhiteCards(string packKey)
-        {
-            WhiteCards = new ObservableCollection<string>(await _cardLoader.GetPackWhiteCardsAsync(packKey).ConfigureAwait(false));
-        }
-
-        #endregion
     }
 }
